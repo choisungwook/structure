39c42471445781a5f23939e5c0a0513a4c6f18b5
choisungwook <kgg1959@naver.com>
2015-04-25 09:52:06 +0900

    Update Divisor.c

--- Algorithm/Divisor/Divisor.c
+++ Algorithm/Divisor/Divisor.c
@@ -2,15 +2,13 @@
 
 int main()
 {
-	int end = 44;
+	int end = 10;
 
 	for(int i=1; i*i<=end; i++){
 		if(end % i == 0){
 			printf("%d ",i);
 			printf("%d ",end / i);
 		}
-		if(i*i == end)
-			continue;
 
 	}
 	return 0;
