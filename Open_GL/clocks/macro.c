c856c1f1ae64f234631e419d2505e40e71c83b90
choisungwook <kgg1959@naver.com>
2015-06-04 22:58:46 +0900

    Update macro.c

--- Open_GL/clocks/macro.c
+++ Open_GL/clocks/macro.c
@@ -1,36 +1,68 @@
 #include <stdio.h>
+#include <math.h>
 
+int bufferX[1000];
+int bufferY[1000];
+int xindex, yindex;
 
 
 int main()
 {
-	int endY = -35, endX = 0;
-	int result = 0;
 	FILE *fout = fopen("out.txt", "w");
+	int x = 0, y = -60;
+	int end = -40;
+	int len = abs(end - y);
 	
-	fprintf(fout, "{");
-	for (int i = -60; i <= endY; i++)
-	{
-		if (i == endY)
-			fprintf(fout, "%d", i);
-		else
-			fprintf(fout, "%d, ", i);		
-		result++;
-	}
 	
-	fprintf(fout, "}\n");	
+	/////////// X --> ///////////////
+	for (int i = 0; i < len; i++)
+		bufferX[xindex++] = i;
 
-	////////////////// X //////////////////
+	for (int i = len - 1; i >= 0; i--)
+		bufferX[xindex++] = i;
+
+	/////////// X <-- ///////////////
+	for (int i = 1; i < len; i++)
+		bufferX[xindex++] = -i;
+
+	for (int i = len - 1; i >= 1; i--)
+		bufferX[xindex++] = -i;
+
+	/////////// Y --> ///////////////
+	for (int i = 0; i < len; i++)
+		bufferY[yindex++] = y + i;
+
+	for (int i = len - 1; i >= 0; i--)
+		bufferY[yindex++] = y + i;
+
+	/////////// Y <-- ///////////////
+	for (int i = 0; i < len; i++)
+		bufferY[yindex++] = y + i;
+
+	for (int i = len - 1; i >= 0; i--)
+		bufferY[yindex++] = y + i;
+
 	fprintf(fout, "{");
-	for (int i = 0; i < endX+result; i++)
+	for (int i = 0; i < xindex; i++)
 	{
-		if (i == endX + result-1)
-			fprintf(fout, "%d", i);
+		if (i==xindex-1)
+			fprintf(fout, "%d", bufferX[i]);
 		else
-			fprintf(fout, "%d, ", i);		
+			fprintf(fout, "%d, ", bufferX[i]);
 	}
 	fprintf(fout, "}\n");
-	fprintf(fout, "%d\n", result);
+
+	fprintf(fout, "{");
+	for (int i = 0; i < yindex; i++)
+	{
+		if (i == yindex - 1)
+			fprintf(fout, "%d", bufferY[i]);
+		else
+			fprintf(fout, "%d, ", bufferY[i]);
+	}
+	fprintf(fout, "}\n");
+
+	fprintf(fout, "%d\n", xindex);
 	fclose(fout);
 	return 0;
 }
