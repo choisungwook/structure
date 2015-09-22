a95f3d234968d9b6648e9d3a4e29942a68c14bc0
choisungwook <kgg1959@naver.com>
2015-09-22 15:24:51 +0900

    Create getsignednum.m

---
+++ Open_GL/math/getsignednum.m
@@ -0,0 +1,8 @@
+%x = [85 90 100 96 87 96];
+%[mean, stdev] = stat(x);
+
+%sprintf('the mean is %4.1f and the standared deviation is %5.2f\n', mean, stdev);
+
+x = [-1 -2 -3; -2 1 3 ; 4 5 6 ];
+
+result = f_signed(x);
