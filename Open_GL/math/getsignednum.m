b0dbfaf24d009d8e1cc8704346efea7be6790746
choisungwook <kgg1959@naver.com>
2015-09-22 15:25:30 +0900

    Update getsignednum.m

--- Open_GL/math/getsignednum.m
+++ Open_GL/math/getsignednum.m
@@ -1,8 +1,9 @@
-%x = [85 90 100 96 87 96];
-%[mean, stdev] = stat(x);
-
-%sprintf('the mean is %4.1f and the standared deviation is %5.2f\n', mean, stdev);
 
 x = [-1 -2 -3; -2 1 3 ; 4 5 6 ];
-
 result = f_signed(x);
+
+
+%함수
+function result = f_signed(x)
+result = sum(x(:)>0);
+
